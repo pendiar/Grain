40e5d11ae5ee319f233b6a7f36009068bd35bb9a
pengding <pengding@sangfor.com.cn>
2017-03-22 18:13:02 +0800

    1

---
+++ web/grain_quasar/src/components/view/AoJian.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- root node required -->
+  <div>
+    <!-- your content -->
+    <div class="layout-padding">
+      <!-- if you want automatic padding -->
+      <div class="card" v-for>
+        <div class="card-title">
+          Card Title
+        </div>
+        <div class="card-content">
+          Card Content
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
